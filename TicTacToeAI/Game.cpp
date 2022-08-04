bef79a7ed363c12eb2024bb9209d7a59e67ce898
Ivan-Krul <kiwiivanya128@gmail.com>
2022-08-04 14:12:09 +0300

    add logic

--- TicTacToeAI/Game.cpp
+++ TicTacToeAI/Game.cpp
@@ -1,5 +1,79 @@
 #include "Game.h"
 #include <sstream>
+void Game::_checkColons(uint8_t& crosses_, uint8_t& rounds_)
+{
+	//colons
+	for(uint8_t iy = 0; iy < _board.size(); iy++)
+	{
+		for(uint8_t ix = 0; ix < _board.size(); ix++)
+		{
+			if(_board.show(ix, iy) == Mark::cross)
+			{
+				crosses_++;
+			}
+			else if(_board.show(ix, iy) == Mark::round)
+			{
+				rounds_++;
+			}
+		}
+		if(crosses_ == _needCheck || rounds_ == _needCheck)
+			return;
+		else
+		{
+			crosses_ = 0;
+			rounds_ = 0;
+		}
+	}
+}
+
+void Game::_checkRows(uint8_t& crosses_, uint8_t& rounds_)
+{
+	//rows
+
+	for(uint8_t ix = 0; ix < _board.size(); ix++)
+	{
+		for(uint8_t iy = 0; iy < _board.size(); iy++)
+		{
+			if(_board.show(ix, iy) == Mark::cross)
+			{
+				crosses_++;
+			}
+			else if(_board.show(ix, iy) == Mark::round)
+			{
+				rounds_++;
+			}
+
+			if(crosses_ == _needCheck || rounds_ == _needCheck)
+				return;
+			else
+			{
+				crosses_ = 0;
+				rounds_ = 0;
+			}
+		}
+	}
+}
+
+void Game::_checkDiagonals(uint8_t& crosses_, uint8_t& rounds_)
+{
+	// diagonals
+
+	for(uint8_t ip = 0; ip < _board.size(); ip++)
+	{
+		for(uint8_t id = 0; id < _board.size(); id++)
+		{
+			if(_board.show(ip+id, ip-id) == Mark::cross)
+			{
+				crosses_++;
+			}
+			else if(_board.show(ip + id, ip - id) == Mark::round)
+			{
+				rounds_++;
+			}
+		}
+	}
+}
+
 Game::Game(uint8_t sizeBoard_,uint8_t needCheck_) : _board(sizeBoard_)
 {
 	for(uint8_t ix = 0; ix < sizeBoard_; ix++)
@@ -44,7 +118,7 @@ std::string Game::output()
 	std::stringstream str;
 	str << "turn: " << (_isCrossTurn ? "cross" : "round") << '\n';
 
-	//board
+	//border
 	str << bordCorner;
 	for(uint8_t ix = 0; ix < _board.size(); ix++)
 	{
@@ -63,7 +137,7 @@ std::string Game::output()
 		str << (needSpace ? " " : "") << bordVertical << "\n";
 	}
 
-	//board
+	//border
 	str << bordCorner;
 	for(uint8_t ix = 0; ix < _board.size(); ix++)
 	{
@@ -71,4 +145,27 @@ std::string Game::output()
 	}
 	str << (needSpace ? " " : "") << bordCorner << "\n";
 	return str.str();
+}
+
+Mark Game::checkWhoWin()
+{
+	// we have board with marks ...
+	// ... and need check this, who is winning
+	// we will be checking all colons, rows and diagonals on winning
+	
+	uint8_t crosses = 0;
+	uint8_t rounds = 0;
+	_checkColons(crosses, rounds);
+	if(crosses == _needCheck)
+		return Mark::cross;
+	else if(rounds == _needCheck)
+		return Mark::round;
+
+	_checkRows(crosses, rounds);
+	if(crosses == _needCheck)
+		return Mark::cross;
+	else if(rounds == _needCheck)
+		return Mark::round;
+	
+	return Mark::empty;
 }
--- TicTacToeAI/Game.h
+++ TicTacToeAI/Game.h
@@ -6,11 +6,16 @@ class Game
 	Board _board;
 	uint8_t _needCheck;
 	bool _isCrossTurn;
+
+	void _checkColons(uint8_t& crosses_,uint8_t& rounds_);
+	void _checkRows(uint8_t& crosses_, uint8_t& rounds_);
+	void _checkDiagonals(uint8_t& crosses_, uint8_t& rounds_);
 public:
 	Game(uint8_t sizeBoard_,uint8_t needCheck_);
 	bool isCrossTurn();
 	const Board show();
 	void turn(uint8_t x_, uint8_t y);
 	std::string output();
+	Mark checkWhoWin();
 };
 
